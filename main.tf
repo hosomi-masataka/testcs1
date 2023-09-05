5a5e40749e54a661674bf0b51b9ffb26d7fb63eb
hosomi-masataka <112477402+hosomi-masataka@users.noreply.github.com>
2023-09-05 19:53:40 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
+terraform {
+  cloud {
+    organization = "PANASONIC-CONNECT-IaC"
+    
+    workspaces {
+      name = "testcs1"
+    }
+  }
+}
+ 
+#リージョンの設定
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region = "ap-northeast-1"
 }
-
-resource "aws_vpc" "test-vpc" {
-  cidr_block = "10.0.0.0/16"
+ 
+#VPC作成
+resource "aws_vpc" "main" {
+  cidr_block              = "10.0.0.0/16"
+  enable_dns_hostnames = true
+ 
   tags = {
-    Name = "test-vpc"
+    Name = "terraformcloud_vpc"
   }
 }
